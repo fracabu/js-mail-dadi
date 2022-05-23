8d2742c8be5a4f8134799d6aec614978a1358e2f
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-23 18:19:13 +0200

    modifiche

---
+++ dadi/script.js
@@ -0,0 +1,24 @@
+//Generare un numero casuale da 1 a 6
+
+const primoNumeroCasuale = Math.floor(Math.random() * 6) + 1 ;
+const secondoNumeroCasuale = Math.floor(Math.random() * 6) + 1 ;
+
+//condizione vincitore
+
+if(primoNumeroCasuale > secondoNumeroCasuale){
+    document.querySelector ('h1').innerHTML = 'giocatore uno vince';
+
+}else if(primoNumeroCasuale < secondoNumeroCasuale){
+    document.querySelector ('h1').innerHTML = 'giocatore due vince';
+
+}else{
+    document.querySelector ('h1').innerHTML = 'Pareggio!'
+
+}
+
+console.log(primoNumeroCasuale,secondoNumeroCasuale);
+
+
+
+
+
